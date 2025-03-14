fb41e7c3bfaa007a1d2405bd308efcc584ee38b6
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 14:42:37 +0600

    Lab12 #issue2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
+namespace TextEditorApp
+{
+    public class Program
+    {
+        static void main(string[] args)
+        {
+            TextEditor editor = new TextEditor();
+            editor.SetContent("Hello World");
+            editor.SetCursorPosition(5);
+            editor.AddSelection("World");
+            editor.DisplayState();
+        }
+    }
+
+}
