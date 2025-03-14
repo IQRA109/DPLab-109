62af3dd648b38b8f1d464e149d3448af16991567
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 14:07:51 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
 
-namespace TextEditorapp
-{
-    public class Program
-    {
-        static void main(string[] args)
-        {
-            Console.WriteLine("The app is started.");
-        }
-    }
-
-}
